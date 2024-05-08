723c0fc701f2e9d5475d6feab9cb98dee2dcb7a0
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-05-08 21:50:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function mincost(arr)
 { 
 //write your code here
+
+	let tc = 0;
+	arr.sort((a,b) => b-a);	
+
+	while(arr.length > 1){
+		let cc = arr[arr.length-1] + arr[arr.length-2];
+		tc+=cc;
+
+		arr.pop();
+		arr.pop();
+
+		arr.push(cc);
+
+		arr.sort((a,b) => b-a);
+	}
+
+	return tc;
+		
+		
+	}
 // return the min cost
   
 }
